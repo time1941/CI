fe5f743b0447d322a20aa1693ea3138b4f602729
time1941 <time254133@gmail.com>
2020-03-01 18:19:45 +0700

    addfile

---
+++ application/models/User_model.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_model extends CI_Model {
+	public function get_user_data($username)
+    {
+        $this->db->select('id,username,password');
+        $this->db->from('users');
+        $this->db->where('username',$username);
+        $query = $this->db->get();
+        return $query->row();  //ตัวเดียว รอบเดียว
+        //$query->result(หลายตัวใช้ตัวนี้);
+
+    }
+}
